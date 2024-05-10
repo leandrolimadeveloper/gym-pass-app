050f5590525441587a100e8e440056aa6a7a1157
leandrolimadeveloper <leandroslimasr@gmail.com>
2024-05-10 00:22:33 -0300

    Primeiro teste unitário

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,7 +1,38 @@
-import { expect, test } from 'vitest'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register-use-case'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-    const name = 'Leonardo'
+describe('Register Use Case', () => {
+    it('should hash user password upon registration', async () => {
+        const registerUseCase = new RegisterUseCase({
+            async findByEmail(email) {
+                return null
+            },
+            
+            async create(data) {
+                return {
+                    id: 'user-1',
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    created_at: new Date()
+                }
+            },
+        })
 
-    expect(name).toBe('Leonardo')
+        const { user } = await registerUseCase.execute({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password: '123456'
+        })
+
+        console.log(user.password_hash)
+
+        const isPasswordCorrectlyHashed = await compare(
+            '123456',
+            user.password_hash
+        )
+
+        expect(isPasswordCorrectlyHashed).toBe(true)
+    })
 })
