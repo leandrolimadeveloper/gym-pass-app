520098e582415d19d15c5bb8085d2aac93be280c
leandrolimadeveloper <leandroslimasr@gmail.com>
2024-05-12 15:49:13 -0300

    Caso de uso de perfil

---
+++ src/use-cases/get-user-profile-use-case.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { GetUserProfileUseCase } from './get-user-profile-use-case'
+import { hash } from 'bcryptjs'
+import { ResourceNotFound } from './errors/resourece-not-found-error'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile', async () => {
+    beforeEach(() => {
+        inMemoryUsersRepository = new InMemoryUsersRepository()
+        sut = new GetUserProfileUseCase(inMemoryUsersRepository)
+    })
+    
+    it('should be able to get a user profile', async () => {
+        const createdUser = await inMemoryUsersRepository.create({
+            name: 'John Doe',
+            email: 'johndoe@example.com',
+            password_hash: await hash('123456', 6)
+        }) 
+
+        console.log(createdUser)
+        
+        const {user} = await sut.execute({
+            userId: createdUser.id
+        })
+    
+        expect(user.name).toEqual('John Doe')
+    })
+
+    it('should not be able to get a user profile', () => {
+        expect(async () => 
+            await sut.execute({
+                userId: 'id-not-exists'
+            })
+        ).rejects.toBeInstanceOf(ResourceNotFound)
+    })
+
+})
