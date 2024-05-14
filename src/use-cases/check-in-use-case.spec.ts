a3cbcd2c96c317542f4d544dd0676de5432e3b80
leandrolimadeveloper <leandroslimasr@gmail.com>
2024-05-13 22:48:26 -0300

    TDD & Mocking

--- src/use-cases/check-in-use-case.spec.ts
+++ src/use-cases/check-in-use-case.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-checkins-repository'
 import { CheckInUseCase } from './check-in-use-case'
 
@@ -9,12 +9,55 @@ describe('Check-in Use Case', async () => {
     beforeEach(() => {
         inMemoryCheckInsRepository = new InMemoryCheckInsRepository()
         sut = new CheckInUseCase(inMemoryCheckInsRepository)
+
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        vi.useRealTimers()
     })
 
     it('should be able to check in a user', async () => {
+        
         const { checkIn } = await sut.execute({
             userId: 'user-01',
             gymId: 'gym-01'
+        })
+
+        expect(checkIn.id).toEqual(expect.any(String))
+    })
+
+    it('should not be able to check in twice in the same day', async () => {
+        vi.setSystemTime(new Date(2023, 0, 1, 8, 0, 0))
+        
+        const { checkIn } = await sut.execute({
+            userId: 'user-01',
+            gymId: 'gym-01'
+        })
+
+        console.log(checkIn.created_at)
+
+        await expect(() =>
+            sut.execute({
+                userId: 'user-01',
+                gymId: 'gym-01'
+            })
+        ).rejects.toBeInstanceOf(Error)
+    })
+
+    it('should be able to check in twice but in different days', async () => {
+        vi.setSystemTime(new Date(2023, 0, 1, 8, 0, 0))
+
+        await sut.execute({
+            gymId: 'gym-01',
+            userId: 'user-01'
+        })
+
+        vi.setSystemTime(new Date(2023, 0, 2, 8, 0, 0))
+
+        const { checkIn } = await sut.execute({
+            gymId: 'gym-01',
+            userId: 'user-01'
         })
 
         expect(checkIn.id).toEqual(expect.any(String))
--- src/use-cases/check-in-use-case.ts
+++ src/use-cases/check-in-use-case.ts
@@ -16,6 +16,15 @@ export class CheckInUseCase {
     ) { }
 
     async execute({ userId, gymId }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+        const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
+            userId,
+            new Date()
+        )
+
+        if (checkInOnSameDay) {
+            throw new Error()
+        }
+        
         const checkIn = await this.checkInsRepository.create({
             user_Id: userId,
             gym_id: gymId
